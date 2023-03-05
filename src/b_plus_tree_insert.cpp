02a8ca9e47000890514face1f0f841825172ccd9
Kris <krischolakov33@gmail.com>
2023-03-05 12:45:05 +0800

    fixxing

--- src/b_plus_tree_insert.cpp
+++ src/b_plus_tree_insert.cpp
@@ -25,7 +25,7 @@ void BPlusTree::insert(Address recordAddress, int key) {
         root->numKeys = 1;
         root->pointers[0] = LLNodeAddress;
         // Save the root node to the indexes storage
-        rootAddress = indexes->saveRecordToStorage(root, nodeSize).blockAddress;
+        rootAddress = indexes->saveRecordToStorage( root, nodeSize).blockAddress;
 
     }
         // Else if root exists already, traverse the nodes to find the proper place to insert the key.
@@ -52,20 +52,16 @@ void BPlusTree::insert(Address recordAddress, int key) {
                 if (key < current->keys[i]) {
                     // Load the new node from the storage
                     nextNode = (TreeNode *) indexes->loadRecordFromStorage(current->pointers[i], nodeSize);
-                    // Set the current node to be the newly found one
-                    currentAddress = current->pointers[i].blockAddress;
-                    current = nextNode;
-                    break;
                 }
-                // Else if the key is larger than all the keys in the current node, get the last assigned pointer
-                if (i == current->numKeys - 1) {
+                    // Else if the key is larger than all the keys in the current node, get the last assigned pointer
+                else if (i == current->numKeys - 1) {
                     // Load the new node from the storage
                     nextNode = (TreeNode *) indexes->loadRecordFromStorage(current->pointers[i + 1], nodeSize);
-                    // Set the current node to be the newly found one
-                    currentAddress = current->pointers[i+1].blockAddress;
-                    current = nextNode;
-                    break;
                 }
+                // Set the current node to be the newly found one
+                currentAddress = current->pointers[i].blockAddress;
+                current = nextNode;
+                break;
             }
         }
 
